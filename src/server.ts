ad181a4b046ad5413d4db66eb16124e8ebf68087
Rafael Anacleto <rafael.anacleto12@gmail.com>
2023-04-07 15:47:05 -0300

    feat: add error handling method in api

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import 'express-async-errors';
+import express, { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import { router } from './app/routes/routes';
 import mongoose from 'mongoose';
 
@@ -9,6 +10,26 @@ mongoose
 
     app.use(express.json());
     app.use(router);
+
+    app.use(
+      (
+        error: ErrorRequestHandler,
+        request: Request,
+        response: Response,
+        next: NextFunction
+      ) => {
+        if (error instanceof Error) {
+          return response.status(400).json({
+            message: error.message
+          });
+        }
+
+        return response.sendStatus(500).json({
+          status: 'error',
+          message: `Internal server error - ${error}`
+        });
+      }
+    );
 
     app.listen(3333, () => {
       console.log('🚀️ Server is running on http://localhost:3333');
