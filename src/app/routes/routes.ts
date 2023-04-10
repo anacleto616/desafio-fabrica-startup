a8b04b67ee3dfa9f42ec524fdd5432cb5a606e39
Rafael Anacleto <rafael.anacleto12@gmail.com>
2023-04-10 19:56:22 -0300

    feat: add caching in api

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import apicache from 'apicache';
+
 import AuthController from '../controllers/AuthController';
 import AuthMiddleware from '../middlewares/AuthMiddleware';
 import CategoryController from '../controllers/CategoryController';
 import ProductController from '../controllers/ProductController';
 
 export const router = Router();
+const cache = apicache.middleware;
 
 // Login
 router.post('/auth/login', AuthController.auth);
@@ -12,11 +15,11 @@ router.post('/auth/login', AuthController.auth);
 router.use(AuthMiddleware.authenticationMiddleware);
 
 // Category
-router.get('/categories', CategoryController.index);
+router.get('/categories', cache('5 minutes'), CategoryController.index);
 
 // Product
 router.post('/products', ProductController.store);
-router.get('/products', ProductController.index);
+router.get('/products', cache('2 minutes'), ProductController.index);
 router.get('/products/:id', ProductController.show);
 router.patch('/products/:id', ProductController.update);
 router.delete('/products/:id', ProductController.delete);
